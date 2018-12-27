2461c93e553fd2e6f3903dad611ac77d72eb2753
Leslie Magnafuzz <dpolya.w@gmail.com>
2018-12-27 17:35:04 +0100

    Added Vector solution.

--- ch-06-exercises/vector.js
+++ ch-06-exercises/vector.js
@@ -15,3 +15,28 @@
     // → Vec{x: -1, y: -1}
     console.log(new Vec(3, 4).length);
  */
+
+class Vec {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    get length() {
+        return Math.sqrt(this.x * this.x + this.y * this.y);
+    }
+
+    plus(vector) {
+        return new Vec(this.x + vector.x, this.y + vector.y);
+    }
+
+    minus(vector) {
+        return new Vec(this.x - vector.x, this.y - vector.y);
+    }
+}
+
+let vector1 = new Vec(2, 3);
+let vector2 = new Vec(4, 5);
+console.log(vector1.plus(vector2));
+console.log(vector1.minus(vector2));
+console.log(vector1.length);
